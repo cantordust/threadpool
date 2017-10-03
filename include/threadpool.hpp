8c4631db6127309c9f5f897577ea979239db83ac
Cantor Dust <1425730-cantordust@users.noreply.gitlab.com>
2017-10-03 22:54:59 +1100

    Added wait() and stop() members.

--- include/threadpool.hpp
+++ include/threadpool.hpp
@@ -22,231 +22,266 @@ namespace Async
 
 	class ThreadPool
 	{
-		private:
+	private:
 
-			struct
+		struct
+		{
+			uint assigned = 0;
+			uint received = 0;
+
+			/// Task queue
+			std::queue<std::function<void()>> queue;
+
+			/// Condition variable used for signalling
+			/// other threads that the processing has finished.
+			std::condition_variable finished;
+		} tasks;
+
+		struct
+		{
+			uint max = 0;
+			uint count = 0;
+			std::condition_variable semaphore;
+			std::condition_variable ready;
+		} workers;
+
+		/// Stop accepting new tasks.
+		bool halt;
+
+		/// Used to indicate that we want to kill some threads
+		bool prune;
+
+		/// Kill switch indicating that it is
+		/// OK to destroy the ThreadPool object
+		std::condition_variable kill_switch;
+
+		/// All shared data should be modified after locking this mutex
+		std::mutex mtx;
+
+		inline void add_worker()
+		{
+			std::thread([&]
 			{
-					uint assigned = 0;
-					uint received = 0;
+				ulock lk(mtx);
+				uint worker_id(++workers.count);
 
-					/// Task queue
-					std::queue<std::function<void()>> queue;
-
-					/// Condition variable used for signalling
-					/// other threads that the processing has finished.
-					std::condition_variable finished;
-			} tasks;
-
-			struct
-			{
-					uint max = 0;
-					uint count = 0;
-					std::condition_variable semaphore;
-					std::condition_variable ready;
-			} workers;
-
-			/// Process all remaing tasks and
-			/// stop accepting new ones
-			bool halt;
-
-			/// Used to indicate that we want to kill some threads
-			bool prune;
-
-			/// Kill switch indicating that it is
-			/// OK to destroy the ThreadPool object
-			std::condition_variable kill_switch;
-
-			/// All shared data should be modified after locking this mutex
-			std::mutex mtx;
-
-			inline void add_worker()
-			{
-				std::thread([&]
-				{
-					uint worker_id(0);
-					ulock lk(mtx);
-					worker_id = ++workers.count;
-#ifdef DLOG_IN_THREADPOOL
-					dlog() << "\tWorker in thread " << std::this_thread::get_id() << " ready";
+#ifdef TP_DEBUG
+				dlog() << "\tWorker " << worker_id << " in thread " << std::this_thread::get_id() << " ready";
 #endif
-					workers.ready.notify_one();
+				workers.ready.notify_one();
 
-					while (true)
+				while (true)
+				{
+					/// Block execution until we have something to process
+					workers.semaphore.wait(lk, [&]{ return (prune || halt || !tasks.queue.empty()); });
+					if (halt && tasks.queue.empty())
 					{
-						/// Block execution until we have something to process
-						workers.semaphore.wait(lk, [&]{ return (prune || halt || !tasks.queue.empty()); });
-						if (halt && tasks.queue.empty())
+						break;
+					}
+					else if (prune)
+					{
+						if (workers.count > workers.max)
 						{
 							break;
 						}
-						else if (prune)
+						else
 						{
-							if (workers.count > workers.max)
-							{
-								break;
-							}
-							else
-							{
-								prune = false;
-								workers.ready.notify_one();
-							}
+							prune = false;
+							workers.ready.notify_one();
 						}
-						else if (!tasks.queue.empty())
+					}
+					else if (!tasks.queue.empty())
+					{
+						std::function<void()> function(std::move(tasks.queue.front()));
+						tasks.queue.pop();
+						++tasks.assigned;
+#ifdef TP_DEBUG
+						dlog() << tasks.assigned << " task(s) assigned (" << tasks.queue.size() << " enqueued)";
+#endif
+						lk.unlock();
+
+						/// Execute the task
+						function();
+
+						lk.lock();
+
+						--tasks.assigned;
+#ifdef TP_DEBUG
+						dlog() << tasks.assigned << " task(s) assigned (" << tasks.queue.size() << " enqueued)";
+#endif
+						/// Notify all waiting threads that
+						/// we have processed all tasks.
+						if (tasks.queue.empty() &&
+							tasks.assigned == 0)
 						{
-							std::function<void()> function(std::move(tasks.queue.front()));
-							tasks.queue.pop();
-							++tasks.assigned;
-#ifdef DLOG_IN_THREADPOOL
-							dlog() << tasks.assigned << " task(s) assigned (" << tasks.queue.size() << " enqueued)";
-#endif
-							lk.unlock();
-
-							/// Grab the topmost task and execute it
-							function();
-
-							lk.lock();
-
-							--tasks.assigned;
-#ifdef DLOG_IN_THREADPOOL
-							dlog() << tasks.assigned << " task(s) assigned (" << tasks.queue.size() << " enqueued)";
-#endif
-							/// Notify all waiting threads that
-							/// we have processed all tasks.
-							if (tasks.queue.size() == 0 &&
-								tasks.assigned == 0)
-							{
-#ifdef DLOG_IN_THREADPOOL
+#ifdef TP_DEBUG
 							dlog() << "Signalling that all tasks have been processed...";
 #endif
-								tasks.finished.notify_all();
-							}
+							tasks.finished.notify_all();
 						}
 					}
-					--workers.count;
-					workers.ready.notify_one();
-					if (halt)
-					{
-						kill_switch.notify_one();
-					}
-#ifdef DLOG_IN_THREADPOOL
-					dlog() << "\tWorker in thread " << std::this_thread::get_id() << " exiting";
-#endif
-				}).detach();
-			}
+				}
 
-		public:
+				--workers.count;
 
-			ThreadPool(const uint _pool_size = std::thread::hardware_concurrency())
-				:
-				  halt(false),
-				  prune(false)
-			{
-				resize(_pool_size);
-			}
-
-			~ThreadPool()
-			{
-#ifdef DLOG_IN_THREADPOOL
-				dlog() << "Notifying all threads that ThreadPool has reached EOL...";
-#endif
-				ulock lk(mtx);
-				halt = true;
-				workers.semaphore.notify_all();
-
-				/// Make sure that the queue is empty and there are no processes assigned
-				kill_switch.wait(lk, [&]{ return (tasks.assigned == 0 && workers.count == 0); });
-#ifdef DLOG_IN_THREADPOOL
-				dlog() << tasks.received << " task(s) completed successfully!";
-#endif
-			}
-
-			template<typename F, typename ... Args>
-			std::future<typename std::result_of<F(Args...)>::type> enqueue(F&& _f, Args&&... _args)
-			{
-				using ret_t = typename std::result_of<F(Args...)>::type;
-
-				auto task(std::make_shared<std::packaged_task<ret_t()>>(std::bind(std::forward<F>(_f), std::forward<Args>(_args)...)));
-				std::future<ret_t> result = task->get_future();
-
+				workers.ready.notify_one();
+				if (halt)
 				{
-					glock lk(mtx);
+					kill_switch.notify_one();
+				}
+#ifdef TP_DEBUG
+				dlog() << "\tWorker in thread " << std::this_thread::get_id() << " exiting";
+#endif
+			}).detach();
+		}
+
+	public:
+
+		ThreadPool(const uint _pool_size = std::thread::hardware_concurrency())
+			:
+			  halt(false),
+			  prune(false)
+		{
+			resize(_pool_size);
+		}
+
+		~ThreadPool()
+		{
+#ifdef TP_DEBUG
+			dlog() << "Notifying all threads that threadpool has reached EOL...";
+#endif
+			ulock lk(mtx);
+			halt = true;
+			workers.semaphore.notify_all();
+
+			/// Make sure that the queue is empty and there are no processes assigned
+			kill_switch.wait(lk, [&]
+			{
+				return (tasks.assigned == 0 && tasks.queue.empty() && workers.count == 0);
+			});
+			tasks.finished.notify_all();
+
+#ifdef TP_DEBUG
+			dlog() << tasks.received << " task(s) completed successfully!";
+#endif
+		}
+
+		template<typename F, typename ... Args>
+		std::future<typename std::result_of<F(Args...)>::type> enqueue(F&& _f, Args&&... _args)
+		{
+			using ret_t = typename std::result_of<F(Args...)>::type;
+
+			auto task(std::make_shared<std::packaged_task<ret_t()>>(std::bind(std::forward<F>(_f), std::forward<Args>(_args)...)));
+			std::future<ret_t> result = task->get_future();
+
+			{
+				glock lk(mtx);
+				if (!halt)
+				{
 					++tasks.received;
 					tasks.queue.emplace([=]{ (*task)(); });
-#ifdef DLOG_IN_THREADPOOL
+#ifdef TP_DEBUG
 					dlog() << "New task received (" << tasks.received << " in total), " << tasks.queue.size() << " task(s) enqueued";
 #endif
 					workers.semaphore.notify_one();
 				}
-
-				return result;
-			}
-
-			inline void resize(const uint _pool_size)
-			{
-				uint new_pool_size(_pool_size);
-				if (new_pool_size == workers.max)
-				{
-					return;
-				}
-				else if (new_pool_size == 0)
-				{
-					new_pool_size = std::thread::hardware_concurrency();
-				}
-
-				ulock lk(mtx);
-				workers.max = new_pool_size;
-				if (workers.max > workers.count)
-				{
-					for (uint i = 0; i < workers.max - workers.count; ++i)
-					{
-						add_worker();
-					}
-				}
+#ifdef TP_DEBUG
 				else
 				{
-					prune = true;
-					workers.semaphore.notify_all();
+					dlog() << "Threadpool stopped, not accepting new tasks.";
 				}
-				workers.ready.wait(lk, [&]{ return workers.count == workers.max; });
+#endif
 			}
 
-			inline void stop()
+			return result;
+		}
+
+		inline void resize(const uint _pool_size)
+		{
+			ulock lk(mtx);
+			if (halt)
 			{
-				/// Stop the thread pool,
-				/// empty the queue and
-				/// kill all the threads
-				glock lk(mtx);
-				halt = true;
-				while (!tasks.queue.empty())
+#ifdef TP_DEBUG
+				dlog() << "Threadpool stopped, resizing not allowed.";
+#endif
+				return;
+			}
+
+			uint new_pool_size(_pool_size);
+			if (new_pool_size == workers.max)
+			{
+				return;
+			}
+			else if (new_pool_size == 0)
+			{
+				new_pool_size = std::thread::hardware_concurrency();
+			}
+
+			workers.max = new_pool_size;
+			if (workers.max > workers.count)
+			{
+				for (uint i = 0; i < workers.max - workers.count; ++i)
 				{
-					tasks.queue.pop();
+					add_worker();
 				}
-				tasks.assigned = 0;
+			}
+			else
+			{
+				prune = true;
 				workers.semaphore.notify_all();
-				tasks.finished.notify_all();
 			}
+			workers.ready.wait(lk, [&]{ return workers.count == workers.max; });
+		}
 
-			inline void wait()
-			{
-				ulock lk(mtx);
-				tasks.finished.wait(lk, [&]{ return (tasks.queue.empty() && tasks.assigned == 0); });
-			}
-
-			inline void pause()
-			{
-				/// \todo Pause
-			}
-
-			inline void resume()
-			{
-				/// \todo Resume
-			}
-
-			inline uint get_queue_size()
+		inline void stop()
+		{
+			/// Stop the thread pool,
+			/// empty the queue and
+			/// kill all the threads
 			{
 				glock lk(mtx);
-				return tasks.queue.size();
+				if (halt)
+				{
+#ifdef TP_DEBUG
+					dlog() << "Threadpool already stopped.";
+#endif
+					return;
+				}
 			}
+
+			glock lk(mtx);
+			halt = true;
+			while (!tasks.queue.empty())
+			{
+				tasks.queue.pop();
+			}
+		}
+
+		inline void wait()
+		{
+			ulock lk(mtx);
+			if (halt)
+			{
+				return;
+			}
+			tasks.finished.wait(lk, [&]{ return (tasks.queue.empty() && tasks.assigned == 0); });
+		}
+
+		inline void pause()
+		{
+			/// \todo Pause
+		}
+
+		inline void resume()
+		{
+			/// \todo Resume
+		}
+
+		inline uint get_queue_size()
+		{
+			glock lk(mtx);
+			return tasks.queue.size();
+		}
 	};
 }
 #endif // MAIN_HPP
--- src/example.cpp
+++ src/example.cpp
@@ -1,41 +1,47 @@
 #include "threadpool.hpp"
 
+std::mutex sleep_mtx;
 static std::mt19937_64 rng;
 static std::uniform_int_distribution<uint> d_task(0, 11);
 static std::uniform_int_distribution<uint> d_workers(4, 15);
-static std::uniform_int_distribution<uint> d_task_sleep(100, 5000);
-static std::uniform_int_distribution<uint> d_main_sleep(5000, 25000);
+static std::uniform_int_distribution<uint> d_sleep(100, 5000);
+static std::uniform_real_distribution<double> d_stop(0.0, 1.0);
+
+uint rnd_sleep()
+{
+	Async::glock lk(sleep_mtx);
+	return d_sleep(rng);
+}
 
 void void_void()
 {
-	uint sleep(d_task_sleep(rng));
+	uint sleep(rnd_sleep());
 	std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
 }
 
 uint uint_void()
 {
-	uint sleep(d_task_sleep(rng));
+	uint sleep(rnd_sleep());
 	std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
 	return sleep;
 }
 
 std::string string_void()
 {
-	uint sleep(d_task_sleep(rng));
+	uint sleep(rnd_sleep());
 	std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
 	return std::to_string(sleep);
 }
 
 void void_uint(const uint _num)
 {
-	uint sleep(d_task_sleep(rng));
+	uint sleep(rnd_sleep());
 	std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
 }
 
 using namespace Async;
 
-void schedule(ThreadPool& _tp,
-			  const uint _tasks)
+void schedule(ThreadPool& _tp, const uint _tasks)
 {
 	for (uint t = 1; t <= _tasks; ++t)
 	{
@@ -75,52 +81,39 @@ int main(void)
 	uint main_sleep(0);
 	uint tasks(20);
 	uint iterations(5);
+	uint runs(4);
 
 	for (uint it = 1; it <= iterations; ++it)
 	{
 		dlog() << "\n************ Iteration " << it << "/" << iterations << " ************\n";
 
-		uint workers(4);
+		uint workers(std::thread::hardware_concurrency());
 		ThreadPool tp(workers);
 
-		dlog() << "--> Scheduling " << tasks << " task(s)";
+		for (uint run = 0; run < runs; ++run)
+		{
+			dlog() << "--> Scheduling " << tasks << " task(s)";
+			schedule(tp, tasks);
 
-		schedule(tp, tasks);
+			/// Synchronise
+			dlog() << "Waiting for tasks to complete...";
+			tp.wait();
+			dlog() << "Tasks completed!";
 
-		workers = d_workers(rng);
-		dlog() << "--> Resizing pool to " << workers << " worker(s)";
-		tp.resize(workers);
-		dlog() << "--> Scheduling " << tasks << " task(s)";
-		schedule(tp, tasks);
+			workers = d_workers(rng);
 
-		/// Synchronise
-		dlog() << "Waiting for tasks to complete...";
-		tp.wait();
-		dlog() << "Tasks completed!";
+			dlog() << "--> Resizing pool to " << workers << " worker(s)";
+			tp.resize(workers);
 
-		main_sleep = d_main_sleep(rng);
-		dlog() << "--> Main thread sleeping for " << main_sleep << " ms";
-		std::this_thread::sleep_for(std::chrono::milliseconds(main_sleep));
+			dlog() << "--> Scheduling " << tasks << " task(s)";
+			schedule(tp, tasks);
 
-		workers = d_workers(rng);
-		dlog() << "--> Resizing pool to " << workers << " worker(s)";
-		tp.resize(workers);
-		dlog() << "--> Scheduling " << tasks << " task(s)";
-		schedule(tp, tasks);
-
-		workers = d_workers(rng);
-		dlog() << "--> Resizing pool to " << workers << " worker(s)";
-		tp.resize(workers);
-
-		main_sleep = d_main_sleep(rng);
-		dlog() << "--> Main thread sleeping for " << main_sleep << " ms";
-		std::this_thread::sleep_for(std::chrono::milliseconds(main_sleep));
-
-		workers = d_workers(rng);
-		dlog() << "--> Resizing pool to " << workers << " worker(s)";
-		tp.resize(workers);
-		dlog() << "--> Scheduling " << tasks << " task(s)";
-		schedule(tp, tasks);
+			if (d_stop(rng) < 0.5)
+			{
+				tp.stop();
+				dlog() << "Threadpool stopped.";
+			}
+		}
 
 		dlog() << "--> Waiting for ThreadPool destructor...";
 
